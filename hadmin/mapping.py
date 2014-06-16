53f1a315cf7f8187035aaf05502bd1077b40ab15
Alec Ten Harmsel <alec@alectenharmsel.com>
2014-06-16 12:56:21 -0400

    Update mapping to new format.
    This new format makes it much easier to add to the mapping, which was
    quite insane before.

--- hadmin/mapping.py
+++ hadmin/mapping.py
@@ -4,99 +4,54 @@ class HadoopMapper:
     field_sep = '.'
     mapping = {
             1: {
-                'maxjobs': {
-                    'scheduler': 'mapred.capacity-scheduler.maximum-system-jobs'
+                'queues': {
+                    'admins': 'mapred.queue.' + rep + '.acl-administer-jobs',
+                    'maxcap': 'mapred.capacity-scheduler.queue.' + rep + \
+                            '.maximum-capacity',
+                    'maxtpu': 'mapred.capacity-scheduler.queue.' + rep + \
+                            '.maximum-initialized-active-tasks-per-user',
+                    'mincap': 'mapred.capacity-scheduler.queue.' + rep + \
+                            '.capacity',
+                    'ulim': 'mapred.capacity-scheduler.queue.' + rep + \
+                            '.minimum-user-limit-percent',
+                    'users': 'mapred.queue.' + rep + '.acl-submit-job'
                     },
 
-                'ulim': {
-                    'scheduler': 'mapred.capacity-scheduler.default-minimum-user-limit-percent',
-                    'queues': 'mapred.capacity-scheduler.queue.' + rep + '.minimum-user-limit-percent'
-                    },
-
-                'maxtpq': {
-                    'scheduler': 'mapred.capacity-scheduler.default-maximum-active-tasks-per-queue'
-                    },
-
-                'maxtpu': {
-                    'scheduler': 'mapred.capacity-scheduler.default-maximum-active-tasks-per-user',
-                    'queues': 'mapred.capacity-scheduler.queue.' + rep + '.maximum-initialized-active-tasks-per-user'
-                    },
-
-                'admins': {
-                    'queues': 'mapred.queue.' + rep + '.acl-administer-jobs'
-                    },
-
-                'users': {
-                    'queues': 'mapred.queue.' + rep + '.acl-submit-job'
-                    },
-
-                'mincap': {
-                    'queues': 'mapred.capacity-scheduler.queue.' + rep + '.capacity'
-                    },
-
-                'maxcap': {
-                    'queues': 'mapred.capacity-scheduler.queue.' + rep + '.maximum-capacity'
-                    },
-
-                'mapred.capacity-scheduler.default-init-accept-jobs-factor': {
-                    'scheduler': 'mapred.capacity-scheduler.default-init-accept-jobs-factor'
-                    },
-
-                'mapred.capacity-scheduler.default-user-limit-factor': {
-                    'scheduler': 'mapred.capacity-scheduler.default-user-limit-factor'
-                    },
-
-                'mapred.capacity-scheduler.default-supports-priority': {
-                    'scheduler': 'mapred.capacity-scheduler.default-supports-priority'
-                    },
-
-                'mapred.capacity-scheduler.init-poll-interval': {
-                    'scheduler': 'mapred.capacity-scheduler.init-poll-interval'
-                    },
-
-                'mapred.capacity-scheduler.init-worker-threads': {
-                    'scheduler': 'mapred.capacity-scheduler.init-worker-threads'
+                'scheduler': {
+                    'mapred.capacity-scheduler.default-init-accept-jobs-factor': \
+                        'mapred.capacity-scheduler.default-init-accept-jobs-factor',
+                    'mapred.capacity-scheduler.default-supports-priority': \
+                        'mapred.capacity-scheduler.default-supports-priority',
+                    'mapred.capacity-scheduler.default-user-limit-factor': \
+                        'mapred.capacity-scheduler.default-user-limit-factor',
+                    'mapred.capacity-scheduler.init-poll-interval': \
+                        'mapred.capacity-scheduler.init-poll-interval',
+                    'mapred.capacity-scheduler.init-worker-threads': \
+                        'mapred.capacity-scheduler.init-worker-threads',
+                    'maxjobs': 'mapred.capacity-scheduler.maximum-system-jobs',
+                    'maxtpq': 'mapred.capacity-scheduler.default-maximum-active-tasks-per-queue',
+                    'maxtpu': 'mapred.capacity-scheduler.default-maximum-active-tasks-per-user',
+                    'ulim': 'mapred.capacity-scheduler.default-minimum-user-limit-percent'
                     }
                 },
             2: {
-                'admins': {
-                    'queues': 'yarn.scheduler.capacity.root.' + rep + '.acl_administer_queue'
+                'queues': {
+                    'admins': 'yarn.scheduler.capacity.root.' + rep + '.acl_administer_queue',
+                    'maxcap': 'yarn.scheduler.capacity.root.' + rep + '.maximum-capacity',
+                    'mincap': 'yarn.scheduler.capacity.root.' + rep + '.capacity',
+                    'state': 'yarn.scheduler.capacity.root.' + rep + '.state',
+                    'ulim': 'yarn.scheduler.capacity.root.' + rep + '.user-limit-factor',
+                    'users': 'yarn.scheduler.capacity.root.' + rep + '.acl_submit_applications'
                     },
 
-                'mincap': {
-                    'queues': 'yarn.scheduler.capacity.root.' + rep + '.capacity'
-                    },
-
-                'maxcap': {
-                    'queues': 'yarn.scheduler.capacity.root.' + rep + '.maximum-capacity'
-                    },
-
-                'maxjobs': {
-                    'scheduler': 'yarn.scheduler.capacity.maximum-applications'
-                    },
-
-                'state': {
-                    'queues': 'yarn.scheduler.capacity.root.' + rep + '.state'
-                    },
-
-                'ulim': {
-                    'queues': 'yarn.scheduler.capacity.root.' + rep + '.user-limit-factor'
-                    },
-
-                'users': {
-                    'queues': 'yarn.scheduler.capacity.root.' + rep + '.acl_submit_applications'
-                    },
-
-                'yarn.scheduler.capacity.maximum-am-resource-percent': {
-                    'scheduler': 'yarn.scheduler.capacity.maximum-am-resource-percent'
-                    },
-
-                'yarn.scheduler.capacity.node-locality-delay': {
-                    'scheduler': 'yarn.scheduler.capacity.node-locality-delay'
-                    },
-
-                'yarn.scheduler.capacity.resource-calculator': {
-                    'scheduler': 'yarn.scheduler.capacity.resource-calculator'
+                'scheduler': {
+                    'maxjobs': 'yarn.scheduler.capacity.maximum-applications',
+                    'yarn.scheduler.capacity.maximum-am-resource-percent': \
+                        'yarn.scheduler.capacity.maximum-am-resource-percent',
+                    'yarn.scheduler.capacity.node-locality-delay': \
+                        'yarn.scheduler.capacity.node-locality-delay',
+                    'yarn.scheduler.capacity.resource-calculator': \
+                        'yarn.scheduler.capacity.resource-calculator'
                     }
                 }
             }
@@ -108,28 +63,20 @@ class HadoopMapper:
         map
         """
 
-        ver = args[1]
+        if len(args) != 3:
+            raise KeyError('You must specify a key, version, and owner')
+
         key = args[0]
-        owner = None
-        if len(args) == 3:
-            owner = args[2]
+        ver = args[1]
+        owner = args[2]
 
-        vermap = HadoopMapper.mapping[ver]
+        tmp_map = HadoopMapper.mapping[ver][owner]
 
-        if key not in vermap.keys():
+        if key not in tmp_map.keys():
             raise KeyError('Hadoop version ' + str(ver) + ' does not have' + \
                     ' support for ' + key)
 
-        keymap = vermap[key]
-        if owner is None:
-            if len(keymap.keys()) == 1:
-                owner = list(keymap.keys())[0]
-            else:
-                raise KeyError('You must specify an owner for key ' + key)
-        elif owner not in keymap.keys():
-            raise KeyError('Mapping is screwed up somewhere')
-
-        return keymap[owner]
+        return tmp_map[key]
 
     def versions(self):
         return sorted(HadoopMapper.mapping.keys())
