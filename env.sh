1322149a5f33831ce377106671ba0879b638acbc
Alec Ten Harmsel <alec@alectenharmsel.com>
2014-07-28 15:02:56 -0400

    I'm just so incompetent
    Redirecting errors from env.sh to /dev/null, because duh. Also, using
    zsh on my development box occasionally ruins me, like right now.

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Try and get HAdmin's root directory... so dang hard
-HADMIN_DIR=`dirname $0`
+HADMIN_DIR=`dirname $0 2> /dev/null`
 if [ ! $? -eq 0 ]
 then
-	HADMIN_DIR=`dirname ${BASH_ARGV[0]}`
+	HADMIN_DIR=`dirname ${BASH_ARGV[0]} 2> /dev/null`
 fi
 
 HADMIN_DIR="`pwd`/${HADMIN_DIR}"
