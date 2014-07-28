923f80f4cc8bd75ac36070f7254c687fb03f2d58
Alec Ten Harmsel <alec@alectenharmsel.com>
2014-07-28 15:00:15 -0400

    Bugfixing env.sh again... Didn't know bash sucked so much

--- env.sh
+++ env.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-HADMIN_DIR=`pwd`/`dirname ${0}`
+# Try and get HAdmin's root directory... so dang hard
+HADMIN_DIR=`dirname $0`
+if [ ! $? -eq 0 ]
+then
+	HADMIN_DIR=`dirname ${BASH_ARGV[0]}`
+fi
+
+HADMIN_DIR="`pwd`/${HADMIN_DIR}"
 
 export PYTHONPATH="${HADMIN_DIR}:${PYTHONPATH}"
 export PATH="${HADMIN_DIR}/bin:${PATH}"
