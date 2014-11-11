870d8f73ee1b4584220b09c6ac2b87ccd43b78a0
Alec Ten Harmsel <alec@alectenharmsel.com>
2014-11-10 23:11:11 -0500

    Fix style problems in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import hadmin_test
 
 setup(
-        name = "HAdmin",
-        version = "0.1",
-        packages = find_packages(),
-        author = "Alec Ten Harmsel",
-        author_email = "alec@alectenharmsel.com",
-        description = "A Hadoop configuration manager",
-        url = "http://github.com/trozamon/hadmin",
-        license = "MIT",
-
-        test_suite = "hadmin_test",
-
-        setup_requires = ['flake8']
+    name="HAdmin",
+    version="0.1",
+    packages=find_packages(),
+    author="Alec Ten Harmsel",
+    author_email="alec@alectenharmsel.com",
+    description="A Hadoop configuration manager",
+    url="http://github.com/trozamon/hadmin",
+    license="MIT",
+    test_suite="hadmin_test",
+    setup_requires=['flake8']
 )
