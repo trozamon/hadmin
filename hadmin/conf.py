1bf85aaf3c8ed30b0cc0912589f192b01fcb4d2f
Alec Ten Harmsel <alec@alectenharmsel.com>
2016-06-23 16:18:14 -0400

    conf: fixup docs a bit

--- hadmin/conf.py
+++ hadmin/conf.py
@@ -4,15 +4,6 @@ HAdmin Configuration
 
 Manipulate hadmin's configuration
 
-hadmin's configuration exists in a directory structure like this:
-
-    /
-    |- queues/
-    | |- staff.qa.yml
-    | |- staff.fun.yml
-
-The above configuration would create two queues, staff.qa and staff.fun.
-
 """
 
 
@@ -22,6 +13,9 @@ import yaml
 
 
 class QueueGenerator:
+    """
+    Generate CapacityScheduler queues with YAML!
+    """
 
     LONG_NAME_KEY = '__long_name'
     DONE_KEY = '__done'
