5c86caca493c50212f3bcea02179fda7ee8c5aee
Alec Ten Harmsel <talec@umich.edu>
2016-07-01 12:27:25 -0400

    rest: add RM REST stats capabilities
    Add class to get YARN scheduling and cluster metrics information.
    Refactor common REST handling code into hadmin.rest.Base.

--- hadmin/rest.py
+++ hadmin/rest.py
@@ -12,33 +12,101 @@ except ImportError:
     from httplib import HTTPConnection
 
 
-class NodeManager:
+# NodeManager paths
+NM_INFO_PATH = '/ws/v1/node'
+
+# ResourceManager paths
+RM_METRICS_PATH = '/ws/v1/cluster/metrics'
+RM_SCHEDULER_PATH = '/ws/v1/cluster/scheduler'
+
+
+class Base:
+    """
+    Handles common networking, JSON, etc. for REST interfaces.
+
+    Subclasses must implement a function with the signature '__init__(dict)'
+    and 'load(dict)'.
+    """
+
+    def __init__(self):
+        raise AttributeError("You cannot initialize this class")
+
+    @classmethod
+    def load_from_host(cls, addr, path):
+        conn = HTTPConnection(addr)
+        return cls.load_from_connection(conn, path)
+
+    @classmethod
+    def load_from_hosts(cls, addr, paths):
+        conn = HTTPConnection(addr)
+        return cls.load_from_connections(conn, paths)
+
+    @classmethod
+    def load_from_connection(cls, conn, path):
+        conn.request('GET', path)
+        res = conn.getresponse()
+        if res.status == 200:
+            return cls.load_from_json(res.read())
+
+    @classmethod
+    def load_from_connections(cls, conn, paths):
+        jsons = []
+        for path in paths:
+            conn.request('GET', path)
+            res = conn.getresponse()
+            if res.status == 200:
+                jsons.append(res.read())
+
+    @classmethod
+    def load_from_json(cls, raw_json):
+        tmp = dict()
+
+        try:
+            tmp = json.loads(raw_json)
+        except ValueError:
+            pass
+        except KeyError:
+            pass
+
+        return cls(tmp)
+
+    @classmethod
+    def load_from_jsons(cls, raw_jsons):
+        obj = None
+
+        for raw_json in raw_jsons:
+            tmp = dict()
+
+            try:
+                tmp = json.loads(raw_json)
+            except ValueError:
+                pass
+            except KeyError:
+                pass
+
+            if obj:
+                obj.load(tmp)
+            else:
+                obj = cls(tmp)
+
+        return obj
+
+
+class NodeManager(Base):
     """
     Wrapper around the NodeManager REST interface
 
     Currently supports the node info only, not applications or containers.
     """
 
-    def __init__(self, json_str=''):
-        self.load(json_str)
+    def __init__(self, obj=dict()):
+        self.load(obj)
 
-    def load(self, json_str=''):
-        try:
-            self.data = json.loads(json_str)['nodeInfo']
-        except ValueError:
+    def load(self, data=dict()):
+        if data:
+            self.data = data['nodeInfo']
+        else:
             self.data = dict()
-        except KeyError:
-            self.data = dict()
-
-    def load_from_host(self, addr):
-        conn = HTTPConnection(addr)
-        return self.load_from_connection(conn)
-
-    def load_from_connection(self, conn):
-        conn.request('GET', '/ws/v1/node')
-        res = conn.getresponse()
-        if res.status == 200:
-            self.load(res.read())
 
     def isHealthy(self):
         return self.data['nodeHealthy']
@@ -53,3 +121,113 @@ class NodeManager:
     @property
     def allocated_cores(self):
         return self.data['totalVCoresAllocatedContainers']
+
+
+class Queue:
+    """
+    REST representation of a queue
+
+    Has these properties:
+
+    * absolute_capacity_used - amount of cluster's max capacity it is using
+    * applications - number of applications currently running
+    * capacity_used - amount of this queue's max capacity it is using
+    * containers - Number of containers used. Only leaf queues can have
+                   non-zero values
+    * name - fully qualified name of the queue
+    * resources_used_vcpus - Number of virtual CPUs in use
+    * resources_used_memory_mb - Amount of memory (in MB) in use
+    """
+
+    def __init__(self, queue_dict, parent_name='root'):
+        self.absolute_capacity_used = queue_dict['absoluteUsedCapacity']
+        self.applications = queue_dict['numApplications']
+        self.capacity_used = queue_dict['usedCapacity']
+        self.name = '.'.join([parent_name, queue_dict['queueName']])
+        self.resources_used_memory_mb = queue_dict['resourcesUsed']['memory']
+        self.resources_used_vcpus = queue_dict['resourcesUsed']['vCores']
+
+        if 'numContainers' in queue_dict:
+            self.containers = queue_dict['numContainers']
+        else:
+            self.containers = 0
+
+
+class ResourceManager(Base):
+    """
+    Wrapper around the ResourceManager REST interface.
+
+    Currently supports the metrics and scheduler APIs.
+
+    Properties:
+
+    * apps_completed - Number of completed applications
+    * apps_failed - Number of applications that ended in failure
+    * apps_killed - Number of applications that were killed by a user
+    * apps_pending - Number of waiting-to-run applications
+    * apps_running - Number of running applications
+    * apps_submitted - Number of submitted applications
+    * capacity_max - Maximum cluster capacity
+    * capacity_used - Currently used cluster capacity
+    * memory_mb_allocated - Amount of memory (in MB) allocated for jobs
+    * memory_mb_reserved - Amount of memory (in MB) reserved for future jobs
+    * memory_mb_total - Amount of total cluster memory (in MB)
+    * nodes_active - Number of active (online) nodes
+    * nodes_decommissioned - Number of decommissioned nodes
+    * nodes_total - Number of total (active + decommissioned + unhealthy) nodes
+    * nodes_unhealthy - Number of unhealthy nodes
+    * queues - Array of Queue objects
+    * vcpus_allocated - Number of vCPUs allocated for jobs
+    * vcpus_reserved - Number of vCPUs reserved for future jobs
+    * vcpus_total - Number of total cluster vCPUs
+    """
+
+    def __init__(self, obj=dict()):
+        self.load(obj)
+
+    def load(self, obj=dict()):
+        if 'clusterMetrics' in obj:
+            self.load_metrics(obj)
+        elif 'scheduler' in obj:
+            self.load_scheduler(obj)
+
+    def load_scheduler(self, data=dict()):
+        # Scheduling stuff
+        self._scheduler = data['scheduler']['schedulerInfo']
+        self.capacity_used = self._scheduler['usedCapacity']
+        self.capacity_max = self._scheduler['maxCapacity']
+
+        self.queues = []
+        self._load_subqueues(self._scheduler['queues']['queue'])
+
+    def _load_subqueues(self, queue_list, prefix='root'):
+        for q in queue_list:
+            tmp = Queue(q, prefix)
+            self.queues.append(tmp)
+
+            if 'queues' in q:
+                self._load_subqueues(q['queues']['queue'],
+                                     prefix=tmp.name)
+
+    def load_metrics(self, data=dict()):
+        met = data['clusterMetrics']
+
+        self.apps_completed = met['appsCompleted']
+        self.apps_failed = met['appsFailed']
+        self.apps_killed = met['appsKilled']
+        self.apps_pending = met['appsPending']
+        self.apps_running = met['appsRunning']
+        self.apps_submitted = met['appsSubmitted']
+
+        self.memory_mb_allocated = met['allocatedMB']
+        self.memory_mb_reserved = met['reservedMB']
+        self.memory_mb_total = met['totalMB']
+
+        self.nodes_active = met['activeNodes']
+        self.nodes_decommissioned = met['decommissionedNodes']
+        self.nodes_total = met['totalNodes']
+        self.nodes_unhealthy = met['unhealthyNodes']
+
+        self.vcpus_allocated = met['allocatedVirtualCores']
+        self.vcpus_reserved = met['reservedVirtualCores']
+        self.vcpus_total = met['totalVirtualCores']
--- hadmin/rest_test.py
+++ hadmin/rest_test.py
@@ -1,5 +1,6 @@
 from unittest2 import TestCase
-from hadmin.rest import NodeManager
+from hadmin.rest import NodeManager, ResourceManager
+from hadmin.rest import NM_INFO_PATH
 from hadmin import mock
 
 
@@ -13,14 +14,161 @@ class NodeManagerTest(TestCase):
                          "1/2 local-dirs are bad: /var/hadoop/compute; ")
 
     def setUp(self):
-        self.rest = NodeManager()
-
         with open('data/nodemanager.rest.json') as f:
-            self.rest = NodeManager(f.read())
+            self.rest = NodeManager.load_from_json(f.read())
 
 
 class NodeManagerNetworkTest(NodeManagerTest):
 
     def setUp(self):
-        self.rest = NodeManager()
-        self.rest.load_from_connection(mock.RESTConnectionMock())
+        self.rest = NodeManager.load_from_connection(mock.RESTConnectionMock(),
+                                                     path=NM_INFO_PATH)
+
+
+class ResourceManagerTest(TestCase):
+
+    def testRootUsed(self):
+        self.assertEqual(self.rm.capacity_used, 0.0)
+
+    def testRootMax(self):
+        self.assertEqual(self.rm.capacity_max, 100.0)
+
+    def testRootQueeus(self):
+        self.assertEqual(len(self.rm.queues), 4)
+
+    def testRootQueueNames(self):
+        self.assertEqual(['root.default', 'root.staff', 'root.staff.dev',
+                          'root.staff.qa'],
+                         self.queue_names)
+
+    def testAppsCompleted(self):
+        self.assertEqual(2592, self.rm.apps_completed)
+
+    def testAppsFailed(self):
+        self.assertEqual(0, self.rm.apps_failed)
+
+    def testAppsKilled(self):
+        self.assertEqual(2, self.rm.apps_killed)
+
+    def testAppsPending(self):
+        self.assertEqual(0, self.rm.apps_pending)
+
+    def testAppsRunning(self):
+        self.assertEqual(0, self.rm.apps_running)
+
+    def testAppsSubmitted(self):
+        self.assertEqual(2598, self.rm.apps_submitted)
+
+    def testNodesActive(self):
+        self.assertEqual(12, self.rm.nodes_active)
+
+    def testNodesDecommissioned(self):
+        self.assertEqual(11, self.rm.nodes_decommissioned)
+
+    def testNodesTotal(self):
+        self.assertEqual(59, self.rm.nodes_total)
+
+    def testNodesUnhealthy(self):
+        self.assertEqual(0, self.rm.nodes_unhealthy)
+
+    def testMemAlloc(self):
+        self.assertEqual(0, self.rm.memory_mb_allocated)
+
+    def testMemRes(self):
+        self.assertEqual(0, self.rm.memory_mb_reserved)
+
+    def testMemTotal(self):
+        self.assertEqual(774144, self.rm.memory_mb_total)
+
+    def testCPUsAlloc(self):
+        self.assertEqual(0, self.rm.vcpus_allocated)
+
+    def testCPUsRes(self):
+        self.assertEqual(0, self.rm.vcpus_reserved)
+
+    def testCPUsTotal(self):
+        self.assertEqual(164, self.rm.vcpus_total)
+
+    # 'default' test
+    def testDefaultAbsCapUsed(self):
+        self.assertEqual(self.default.absolute_capacity_used, 2.0)
+
+    def testDefaultApplications(self):
+        self.assertEqual(self.default.applications, 3)
+
+    def testDefaultCapUsed(self):
+        self.assertEqual(self.default.capacity_used, 4.0)
+
+    def testDefaultContainers(self):
+        self.assertEqual(self.default.containers, 6)
+
+    def testDefaultCpus(self):
+        self.assertEqual(self.default.resources_used_vcpus, 6)
+
+    def testDefaultMem(self):
+        self.assertEqual(self.default.resources_used_memory_mb, 8192)
+    # end 'default' test
+
+    # 'staff' test
+    def testStaffAbsCapUsed(self):
+        self.assertEqual(self.staff.absolute_capacity_used, 50.0)
+
+    def testStaffApplications(self):
+        self.assertEqual(self.staff.applications, 5)
+
+    def testStaffCapUsed(self):
+        self.assertEqual(self.staff.capacity_used, 100.0)
+
+    def testStaffContainers(self):
+        self.assertEqual(self.staff.containers, 0)
+
+    def testStaffCpus(self):
+        self.assertEqual(self.staff.resources_used_vcpus, 20)
+
+    def testStaffMem(self):
+        self.assertEqual(self.staff.resources_used_memory_mb, 50000)
+    # end 'staff' test
+
+    # 'staff.dev' test
+    def testStaffDevAbsCapUsed(self):
+        self.assertEqual(self.staff_dev.absolute_capacity_used, 45.0)
+
+    def testStaffDevApplications(self):
+        self.assertEqual(self.staff_dev.applications, 3)
+
+    def testStaffDevCapUsed(self):
+        self.assertEqual(self.staff_dev.capacity_used, 100.0)
+
+    def testStaffDevContainers(self):
+        self.assertEqual(self.staff_dev.containers, 30)
+
+    def testStaffDevCpus(self):
+        self.assertEqual(self.staff_dev.resources_used_vcpus, 15)
+
+    def testStaffDevMem(self):
+        self.assertEqual(self.staff_dev.resources_used_memory_mb, 40000)
+    # end 'staff.dev' test
+
+    def setUp(self):
+        met = ''
+        sched = ''
+        with open('data/resourcemanager.scheduler.json') as f:
+            sched = f.read()
+
+        with open('data/resourcemanager.metrics.json') as f:
+            met = f.read()
+
+        self.rm = ResourceManager.load_from_jsons([sched, met])
+        self.queue_names = []
+
+        for q in self.rm.queues:
+            if q.name == 'root.default':
+                self.default = q
+            elif q.name == 'root.staff':
+                self.staff = q
+            elif q.name == 'root.staff.dev':
+                self.staff_dev = q
+
+            self.queue_names.append(q.name)
+
+        self.queue_names = sorted(self.queue_names)
