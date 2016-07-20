3f07326805956e3cdd6b4e37a8079be8c896ae3d
Alec Ten Harmsel <alec@alectenharmsel.com>
2016-07-20 14:53:10 -0400

    Fix spelling error in setup.py
    Fixes #27

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import hadmin
 
 desc = """HAdmin (Hadoop Admin) is a general hadoop administration tool. It
 allows for creating users, queues, and groups. It also contains utilities for
-exporiting metrics to graphite and influxdb""".replace('\n', ' ')
+exporting metrics to graphite and influxdb""".replace('\n', ' ')
 
 setup(
     name='hadmin',
